e66ccd7ceb515e43dce941bc51441e5a7fab4f57
andrew.buhagiar <andrew.buhagiar.19@um.edu.mt>
2021-11-24 20:03:09 +0100

    Added websocket mesh

--- src/spawner/node/client.js
+++ src/spawner/node/client.js
@@ -1,7 +1,10 @@
 const WebSocket = require('ws');
+const WebSocketServer = require('ws');
 const {spawn, terminate, send} = require('../../actors.js');
 const node = require('./node.json');
 const spawnable = require('./spawnable.js')
+
+let peerSocks = {};
 
 //Init connection actor
 let init = spawn({}, (state, message) => {
@@ -9,11 +12,36 @@ let init = spawn({}, (state, message) => {
     const spawnerSock = new WebSocket(`ws://localhost:8080/?id=${node.name}`);
     spawnerSock.on('open', () => {
         try{
-            send(main, {spawnerSock});
+            //Wait for acknowledgement from server
+            spawnerSock.on('message', message => {
+                if(message.toString() === 'connect'){
+                    //Connect to peers
+                    for(const i in node.peers){
+                        if(i != node.name){
+                            peerSocks[i] = new WebSocket(`ws://localhost:${node.peers[i]}/?id=${node.name}`);
+                        }
+                    }
+
+                    //Send acknowledgement to server that peers have been connected with
+                    spawnerSock.send('connected with peers')
+                    send(main, {spawnerSock});
+                }
+            })
+
+            //Start server for peers
+            const wss = new WebSocketServer.Server({ port: node.peers[node.name] });
+            wss.on('connection', (ws, req) => {
+                const nodeName = req.url.split('id=')[1]
+                ws.on('message', message => {
+                  console.log(`Node ${nodeName}: ${message}`);
+                });
+            }); 
+
+            //Send acknowledgement to spawer
+            spawnerSock.send('initialized')
+
         }catch(err){
             spawnerSock.send(err.toString())
-        }finally{            
-            terminate(init);
         }
     })
 })
@@ -25,6 +53,8 @@ let main = spawn({}, (state, message) => {
         eval(`spawn(${JSON.stringify(actorDef.state)}, spawnable.${actorDef.name})`)
         message.spawnerSock.send(`Spawned ${actorDef.name}`)
     })
+
+    //Execute master code
 })
 
 //Start process
--- src/spawner/spawner.js
+++ src/spawner/spawner.js
@@ -5,18 +5,40 @@ const { exec } = require("child_process");
 const nodes = require('./nodes.json');
 const fs = require('fs');
 
+//Set up websocket ports for local nodes
+let peers = {};
+let connectedPeers = 0;
+let portOffset = 8080
+for(const i in nodes)
+    peers[i] = ++portOffset;
+
 wss.on('connection', (ws, req) => {
     const nodeName = req.url.split('id=')[1]
     ws.on('message', message => {
-      console.log(`Node ${nodeName}: ${message}`);
-    });
-  });
+        console.log(`Node ${nodeName}: ${message}`);
+
+        if(message.toString() === 'initialized'){
+            connectedPeers++;
+            if(Object.keys(peers).length === connectedPeers){
+                wss.clients.forEach(client => {
+                    client.send('connect');
+                });
+            }
+        }
+    });    
+});
 
 //Set up environments for all nodes in nodes.json
 for(const i in nodes){
     exec(`cp -r node/ ${i}/`, () => {
-        fs.appendFileSync(`${i}/node.json`, JSON.stringify({'name': i, 'behaviour': nodes[i]}), err => console.log(err)); 
-        exec(`node ${i}/client.js &`)
+        clientJson = {
+            'name': i, 
+            'behaviour': nodes[i],
+            peers
+        }        
+        
+        fs.appendFileSync(`${i}/node.json`, JSON.stringify(clientJson), err => console.log(err)); 
+        // exec(`node ${i}/client.js &`)
     })    
 }
 
