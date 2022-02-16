09269f0d2343f070af44de546eb014b4995393bc
andrew.buhagiar <andrew.buhagiar.19@um.edu.mt>
2022-02-16 13:59:16 +0100

    Finished distributed ping pong example

--- test/coordinator.js
+++ test/coordinator.js
@@ -3,25 +3,44 @@ const WebSocket = require('ws');
 const wss = new WebSocket.Server({ port: 8080 });
 const worker = new WebSocket('ws://localhost:8081');
 
-ping = spawn({}, (state, behaviour) => {
-    console.log("ping")
+let pong;
+
+ping = spawn({i: 0}, (state, message) => {
+    console.log(message.message)
+    if(state.i < 5){
+        // send(pong, {message: "ping"})
+        // state.i++;
+        setTimeout(() => {
+            send(pong, {message: "ping"})
+            state.i++;
+        }, 1000)    
+    }
 }, "ping")
 
 worker.on('open', () => {
-    const actor = remoteSpawn("test", worker, {}, (state, message) => { 
+    pong = remoteSpawn("test", worker, {ping}, (state, message) => { 
+        console.log(message.message);
         const WebSocket = require('ws');
-        let coordinator = new WebSocket('ws://localhost:8080');
-
-        coordinator.on('open', () => {                       
-            coordinator.send("testing")
-        })
+        const coordinator = new WebSocket('ws://localhost:8080');
+        const {send} = require('../src/actors.js');
+        state.ping.ws = coordinator;
+        coordinator.on('open', () => {           
+            send(state.ping, {message: "pong"});
+            coordinator.close()
+        });
     });
 
-    send(actor, {message: "ping"})
+    send(pong, {message: "ping"})
 });
 
 wss.on('connection', ws => {
     ws.on('message', message => {
-        console.log(message.toString())
+        const messageJson = JSON.parse(message.toString())
+        if("behaviour" in messageJson){
+            spawn(messageJson.state, messageJson.behaviour, messageJson.name)
+        }else{
+            const referredActor = getActor(messageJson.name)
+            send(referredActor, messageJson.message)
+        }
     });
 });
