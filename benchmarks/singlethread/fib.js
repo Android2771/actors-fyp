221bffe7d24e5408b8409392983e114c0adee65b
Android2771 <andrew.buha@gmail.com>
2022-02-25 18:09:37 +0100

    Implemented draft fibonnachi

--- benchmarks/singlethread/fib.js
+++ benchmarks/singlethread/fib.js
@@ -1,2 +1,38 @@
 // Tests incremental actor creation and destruction
 const { init, spawn, spawnRemote, terminate, send, getActor } = require('../../src/actors.js');
+
+const N = 5;    //fibonnachi index
+
+const behaviour = (state, message, self) => {
+    state.received++;
+    switch(state.received){
+        case 1: //Initialisation
+            if(message.value <= 1){
+                send(state.parent, {value: message.value})
+                terminate(self)
+            }else{
+                send(spawn({received: 0, parent: self}, behaviour), {value: message.value-1})
+                send(spawn({received: 0, parent: self}, behaviour), {value: message.value-2})
+            }    
+        break;
+        case 2: //First value received from child
+            state.buffer = message.value;
+        case 3: //Second value received from child
+            send(state.parent, {value: state.buffer + message.value});
+            terminate(self)
+        break;
+    }
+}
+
+const benchmarker = spawn({received: 0}, (state, message, self) => {
+    state.received++;
+    switch(state.received){
+        case 1:
+            send(spawn({parent: self, received: 0}, behaviour), {value: message.value});
+        break;
+        case 2:
+            console.log(message.value)
+    }
+})
+
+send(benchmarker, {value: N})
--- benchmarks/singlethread/fjthrput.js
+++ benchmarks/singlethread/fjthrput.js
@@ -1,2 +1,54 @@
 // Tests messaging throughput
 const { init, spawn, spawnRemote, terminate, send, getActor } = require('../../src/actors.js');
+
+const N = 20000;  //total messages to send to each actor
+const K = 10;       //total number of actors to spawn
+const rounds = 10;
+const actors = []
+
+const benchmarker = spawn({rounds, times: [], actors, messagesToSend: N*K}, (state, message, self) => {
+
+    switch(message.header){
+        case "start":
+            state.start = new Date();
+            for(let i = 0; i < state.messagesToSend; i++){
+                send(state.actors[i % K], {sender: self})
+            }
+        break;
+        case "end":
+            state.end = new Date()
+            const time = state.end.getTime() - state.start.getTime()
+            console.log(`Finished in ${time}ms`);
+            state.times.push(time)
+            state.rounds--;
+            if(state.rounds === 0){
+                const avg = state.times.reduce((a,b) => (a+b)) / state.times.length;
+                console.log(`Average time: ${avg}ms`);
+            }else{
+                send(self, {header: "start"});
+            }
+        break;
+    }
+
+}
+);
+
+for(let i = 0; i < K-1; i++){
+    const actor = spawn({i: 0, N}, (state, message, self) => {
+        if(++state.i >= N)
+            state.i = 0
+    });
+    actors.push(actor)
+}
+
+
+//Final spawned actor replies to benchmarker
+const actor = spawn({i: 0}, (state, message, self) => {
+    if(++state.i >= N){
+        send(message.sender, {header: "end"});
+        state.i = 0
+    }
+});
+actors.push(actor)
+
+send(benchmarker, {header: "start"})
