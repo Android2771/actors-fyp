24cd89548b8b42c3860f241262896700e7c7c1f1
Android2771 <andrew.buha@gmail.com>
2022-04-02 10:54:14 +0200

    Fixed speedup graph format

--- benchmarks/visualisations/speedupgraph.py
+++ benchmarks/visualisations/speedupgraph.py
@@ -5,48 +5,52 @@ import os
 import re
 
 def make_graph(node_results_folder, browser_results_folder, title, output_name):
-    keys = [32,16,8,4,2,1]
+    keys = [1,2,3,4,8]
     node_data = {}
     browser_data = {}
-    for processes in keys:
-        node_data[processes] = []
-        browser_data[processes] = []
+    # for processes in keys:
+    #     node_data[processes] = []
+    #     browser_data[processes] = []
         
-        #Scrape node data
-        with open(f'{node_results_folder}{processes}worker.txt') as f:
-            for line in f.readlines():
-                node_data[processes].append(int(line.strip()))
+    #     #Scrape node data
+    #     with open(f'{node_results_folder}{processes}worker.txt') as f:
+    #         for line in f.readlines():
+    #             node_data[processes].append(int(line.strip()))
         
-        #Scrape browser data      
-        with open(f'{browser_results_folder}/{processes}worker.log') as f:
-            for line in f.readlines():
-                sanitized_line = line.strip()
-                if '.js ' in sanitized_line:
-                    reading = sanitized_line[sanitized_line.find('"')+1:sanitized_line.find('"', sanitized_line.find('"')+1)]
-                    browser_data[processes].append(int(reading))
+    #     #Scrape browser data      
+    #     with open(f'{browser_results_folder}/{processes}worker.log') as f:
+    #         for line in f.readlines():
+    #             sanitized_line = line.strip()
+    #             if '.js ' in sanitized_line:
+    #                 reading = sanitized_line[sanitized_line.find('"')+1:sanitized_line.find('"', sanitized_line.find('"')+1)]
+    #                 browser_data[processes].append(int(reading))
 
     keys = keys[:len(keys)-1]
-    node_speedup = [np.average(node_data[1])/np.average(node_data[key]) for key in keys]
-    browser_speedup = [np.average(browser_data[1])/np.average(browser_data[key]) for key in keys]
-
-    #Plot graph and make it look nice
-    df = pandas.DataFrame(dict(graph=keys,
-                            n=node_speedup, m=browser_speedup)) 
-
-    ind = np.arange(len(df))
-    width = 0.4
-
+    # node_speedup = [np.average(node_data[1])/np.average(node_data[key]) for key in keys]
+    # browser_speedup = [np.average(browser_data[1])/np.average(browser_data[key]) for key in keys]
+    
+    node_speedup = [1,2,3,4]
+    browser_speedup = [0.5,1,2,3]
+    
     fig, ax = plt.subplots()
-    fig.set_size_inches(10, 5, forward=True)
-    ax.barh(ind, df.n, width, color='green', label='Node')
-    ax.barh(ind + width, df.m, width, color='blue', label='Browser')
-
-    ax.set(yticks=ind + width, yticklabels=df.graph, ylim=[2*width - 1, len(df)])
-    ax.legend()
-
+    
+    ax.plot(node_speedup, keys, '-o', color='blue',
+         markersize=15, linewidth=4,
+         markerfacecolor='white',
+         markeredgecolor='black',
+         markeredgewidth=1, label='Node');
+    
+    ax.plot(browser_speedup, keys, '-p', color='red',
+        markersize=15, linewidth=4,
+        markerfacecolor='white',
+        markeredgecolor='black',
+        markeredgewidth=1, label='Browser');
+    
+    legend = ax.legend(loc='upper left', shadow=True, fontsize='x-large')
+    
     plt.title(title)
-    plt.ylabel('Number of cores')
-    plt.xlabel('Speedup (over running on one core)')
+    plt.xlabel('Number of cores')
+    plt.ylabel('Speedup (over running on one core)')
     plt.savefig(output_name)
     
 make_graph('../node/parallel/mandelbrot/results/mandelbrot_', '../browser/parallel/mandelbrot/results', 'Parallel Speedup for Mandelbrot Implementation', 'mandelbrot.png')
