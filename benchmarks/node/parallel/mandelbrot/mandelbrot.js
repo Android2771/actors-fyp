0feef2dc35eae886ac069ca6b7454e6bf6628a27
Android2771 <andrew.buha@gmail.com>
2022-04-07 21:43:20 +0200

    Revised mandelbrot implementation

--- benchmarks/node/parallel/mandelbrot/mandelbrot.js
+++ benchmarks/node/parallel/mandelbrot/mandelbrot.js
@@ -3,8 +3,8 @@ const { init, spawn, spawnRemote, terminate, send, closeConnection } = actors
 import fs from 'fs';
 
 const constants = {
-    width: 12000,
-    height: 8000,
+    width: 400000,
+    height: 80,
     realStart: -2,
     realEnd: 1,
     imaginaryStart: -1,
@@ -13,8 +13,9 @@ const constants = {
 }
 
 const K = parseInt(process.argv.slice(2)[0]);
-const rounds = parseInt(process.argv.slice(2)[1]);
-const output = process.argv.slice(2)[2] === "true"
+const step = parseInt(process.argv.slice(2)[1]);
+const rounds = parseInt(process.argv.slice(2)[2]);
+const output = process.argv.slice(2)[3] === "true";
 
 const rowRendererBehaviour = (state, message, self) => {
     const add = (x, y) => ({re: x.re + y.re, im: x.im + y.im});   
@@ -26,22 +27,26 @@ const rowRendererBehaviour = (state, message, self) => {
         let n = 0;
         while(abs(z) <= 2 && n < state.iterations){
             z = add(mul(z, z), c);
-            n += 1;
+            n++;
         }
         return n;
     };
 
-    const pixelRow = [];
+    const pixelRows = [];
 
-    for(let x = 0; x < state.width; x++){
-        const c = {re: state.realStart + (x / state.width) * (state.realEnd - state.realStart),
-                   im: state.imaginaryStart + (message.y / state.height) * (state.imaginaryEnd - state.imaginaryStart)};
-        const m = mandelbrot(c);
-        const colour = 255 - parseInt(m * 255 / state.iterations);
-        pixelRow.push(colour);
+    for(let y = message.start; y < message.end; y++){
+        const row = [];
+        for(let x = 0; x < state.width; x++){
+            const c = {re: state.realStart + (x / state.width) * (state.realEnd - state.realStart),
+                    im: state.imaginaryStart + (y / state.height) * (state.imaginaryEnd - state.imaginaryStart)};
+            const m = mandelbrot(c);
+            const colour = 255 - parseInt(m * 255 / state.iterations);
+            row.push(colour);
+        }
+        pixelRows.push(row);
     }
 
-    send(message.sender, {header: "ROW", pixelRow, y: message.y, from: self});
+    send(message.sender, {header: "ROWS", pixelRows, start: message.start, from: self});
 };
 
 init('ws://localhost:8080', 0x7FFFFFF, K).then(ready => {    
@@ -50,29 +55,30 @@ init('ws://localhost:8080', 0x7FFFFFF, K).then(ready => {
             switch(message.header){
                 case "START":
                     state.start = new Date();  
-                    //Spawn actors and initial fan out of work
-                    state.nextRow = K;
                     for(let i = 1; i <= K; i++){
                         spawnRemote(i+1, state.constants, state.rowRendererBehaviour).then(actor => {
                             state.actors.push(actor);
-                            send(actor, {y: i, sender: self});  
+                            send(actor, {start: state.nextRow, end: state.nextRow+step, sender: self});  
+                            state.nextRow += step                           
                         });
                     }                
                 break;
-                case "ROW":
-                    state.responses[message.y] = message.pixelRow;
-                    if(++state.receivedRows > state.constants.height){
+                case "ROWS":
+                    state.responses[message.start] = message.pixelRows;
+                    state.receivedRows += step;
+
+                    if(state.receivedRows >= state.constants.height){
                         state.end = new Date();
                         const time = state.end.getTime() - state.start.getTime();
                         console.log(time);
 
-                        for(let i in Object.keys(state.responses))
-                            if(i > 0)
-                                state.image.push(state.responses[i]);
+                        for(let i in state.responses)
+                            for(const row of state.responses[i])
+                                state.image.push(row);
                         
                         if(output)                            
                             fs.writeFile("mandelbrot.json", JSON.stringify(state.image), err => {closeConnection()});                        
-                        else if(--state.rounds != 0){
+                        else if(--state.rounds !== 0){
                             state.responses = {};
                             state.image = [];
                             state.receivedRows = 0;
@@ -80,10 +86,15 @@ init('ws://localhost:8080', 0x7FFFFFF, K).then(ready => {
                             state.actors = [];
                             send(self, {header: "START"});
                         }
-                        else
+                        else{
                             closeConnection();
-                    }else if(state.nextRow++ <= state.constants.height){                              
-                        send(message.from, {y: state.nextRow, sender: self});
+                        }
+                    }else{   
+                        if(state.nextRow !== state.constants.height){           
+                            const end = state.nextRow+step >= state.constants.height ? state.constants.height : state.nextRow+step  
+                            send(message.from, {start: state.nextRow, end, sender: self});
+                            state.nextRow = end;
+                        }
                     }
                 break;
             }
--- benchmarks/node/parallel/mandelbrot/mandelbrot.sh
+++ benchmarks/node/parallel/mandelbrot/mandelbrot.sh
@@ -6,11 +6,15 @@ export NODE_OPTIONS=--max-old-space-size=4192;
 run_benchmark(){
     node ../../../../src/network.js $(($1+1)) 100 &
     sleep 1
-    node mandelbrot_rows.js $1 5 false > results/$2.txt
+    node mandelbrot.js $1 $2 $3 false #> results/mandelbrot_$1worker.txt
     kill %%
 }
 
-run_benchmark 1 "mandelbrot_1worker"
-run_benchmark 2 "mandelbrot_2worker"
-run_benchmark 3 "mandelbrot_3worker"
-run_benchmark 4 "mandelbrot_4worker"
+run_batch(){
+    run_benchmark 1 $1 $2
+    run_benchmark 2 $1 $2
+    run_benchmark 3 $1 $2 
+    run_benchmark 4 $1 $2
+}
+
+run_batch $1 $2
